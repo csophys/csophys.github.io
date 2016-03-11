b61a214799e21407c7098a9dc50f45aa4dbefbe2
csophys <zjsy_cs@163.com>
2016-03-11 13:25:56 +0800

    ok

---
+++ theme/readtheorg/hack.js
@@ -0,0 +1,5 @@
+$(function(){
+    $("#content").prepend('<div id="toggle-sidebar"><a href="#table-of-contents"><h2>Table of Contents</h2></a></div>');
+    $("#table-of-contents h2").html('Table of Contents<a href="#">Close</a>');
+});
+
